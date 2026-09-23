e88b3d353212f671afbb14a5133a1827bb0d8f5f
JosePro <josedesouzapro@gmail.com>
2026-09-23 14:09:59 +0000

    Ajout d'Alembic pour la gestion des migrations de base de données

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
+
 
 from app.database import engine, Base
 from app import models
@@ -13,8 +13,6 @@ from app.routers import auth, routers_management, wallet, admin, notifications, 
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-
-Base.metadata.create_all(bind=engine)
 
 limiter = Limiter(key_func=get_remote_address)
 
